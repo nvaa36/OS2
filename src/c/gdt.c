333456d8c0b7cdf29d4c4c0f71130af499a42c24
Nicole Anderson-Au <nvaa36@gmail.com>
2019-04-13 20:31:27 -0700

    Add gdt code into c. Don't know if it is actually working, but doesn't break.

---
+++ src/c/gdt.c
@@ -0,0 +1,44 @@
+#include "gdt.h"
+
+gdt_entry gdt[NUM_GDT_ENTRIES];
+table_desc table_descriptor;
+
+void setup_gdt() {
+   /* Zero first entry. */
+   memset(gdt, 0, sizeof(gdt_entry));
+   setup_kernel_segment();
+
+   /* Make the table descriptor */
+   lgdt((void *)gdt, sizeof(gdt));
+}
+
+void setup_kernel_segment() {
+   gdt[KERN_IND].limit1 = 0;
+   gdt[KERN_IND].base_addr1 = 0;
+   gdt[KERN_IND].base_addr2 = 0;
+   gdt[KERN_IND].a = 0;
+   gdt[KERN_IND].r = 0;
+   gdt[KERN_IND].c = 0;
+   gdt[KERN_IND].exec = 1;
+   gdt[KERN_IND].one = 1;
+   gdt[KERN_IND].dpl = 0;
+   gdt[KERN_IND].present = 1;
+
+   gdt[KERN_IND].limit2 = 0;
+   gdt[KERN_IND].avail = 0;
+   gdt[KERN_IND].long_mode = 1;
+   gdt[KERN_IND].d = 0;
+   gdt[KERN_IND].g = 0;
+
+   gdt[KERN_IND].base_addr3 = 0;
+}
+
+void lgdt(void* base, uint16_t size) {
+   struct {
+      uint16_t length;
+      void* base;
+   } __attribute__((packed)) GDTR;
+   GDTR.length = size;
+   GDTR.base = base;
+   asm ( "lgdt %0" : : "m"(GDTR) );
+}
