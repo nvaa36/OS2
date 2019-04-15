b0a0b2b0c379116145c609a3e0c3b174ed8f2345
Nicole Anderson-Au <nvaa36@gmail.com>
2019-04-14 22:29:38 -0700

    Add script to generate the isr asms for each code

---
+++ helper_scripts/generate_isrs.py
@@ -0,0 +1,10 @@
+output_file = open("isr.asm", "w")
+
+header = open("isr_header.txt", "r").read()
+output_file.write(header)
+output_file.write("\n")
+
+base = open("isr_base.txt", "r").read()
+
+for i in range(255):
+	output_file.write(base % (i, i // 100 + 30, (i // 10) % 10 + 30, i % 10 + 30) + "\n")
